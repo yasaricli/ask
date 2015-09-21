05a4d3c5dd1d33ab4107248df6d9ad6df863c9d5
Yasar icli <hello@yasaricli.com>
2015-09-21 20:09:49 +0300

    useraccounts form

--- client/lib/config.js
+++ client/lib/config.js
@@ -1,3 +1,5 @@
+var pwd = AccountsTemplates.removeField('password');
+
 Router.configure({
   layoutTemplate: 'layout',
   loadingTemplate: 'loading',
@@ -12,3 +14,44 @@ Router.configure({
     this.next();
   }
 });
+
+AccountsTemplates.configure({
+  enablePasswordChange: true,
+  homeRoutePath: '/',
+  showPlaceholders: false,
+
+  negativeValidation: true,
+  positiveValidation: true,
+  negativeFeedback: false,
+  positiveFeedback: true
+});
+
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('signUp');
+AccountsTemplates.configureRoute('changePwd');
+
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+      _id: "username",
+      type: "text",
+      displayName: "username",
+      required: true,
+      minLength: 5,
+  },
+  {
+      _id: "fullname",
+      type: "text",
+      displayName: "Full name",
+      required: true
+  },
+  {
+      _id: 'email',
+      type: 'email',
+      required: true,
+      displayName: "email",
+      re: /.+@(.+){2,}\.(.+){2,}/,
+      errStr: 'Invalid email',
+  },
+  pwd
+]);
